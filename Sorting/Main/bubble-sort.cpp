dc87fef74b49cd19af662437e7d5ca16f6b8d268
Ha0203 <mr.stsh020303@gmail.com>
2022-06-08 11:57:49 +0700

    optimize buble sort and finish shaker sort

--- Sorting/Main/bubble-sort.cpp
+++ Sorting/Main/bubble-sort.cpp
@@ -4,7 +4,7 @@ void bublesort(int a[], int n) {
 	int k = 0;
 	while (k < n - 1) {
 
-		for (int i = 0; i < n - 1; i++) {
+		for (int i = 0; i < n - 1 - k; i++) {
 			if (a[i] > a[i + 1])
 				swap(a[i], a[i + 1]);
 		}
---
+++ Sorting/Main/shaker-sort.cpp
@@ -0,0 +1,18 @@
+#include"Header.h"
+
+void shakersort(int a[], int n) {
+	int left = 0, right = n - 1;
+	while (right - left > 1) {
+		for (int i = left; i < right; i++) {
+			if (a[i] > a[i + 1])
+				swap(a[i], a[i + 1]);
+		}
+		right--;
+		for (int i = right; i > left; i--)
+		{
+			if (a[i] < a[i - 1])
+				swap(a[i], a[i - 1]);
+		}
+		left++;
+	}
+}
