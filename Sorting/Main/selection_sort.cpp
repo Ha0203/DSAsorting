819de64639cb6ac39dfc01dfe89c04723f24d142
ngkhnghyyy <nkhy21@clc.fitus.edu.vn>
2022-06-09 16:31:40 +0700

    heap_sort p2

--- Sorting/Main/selection_sort.cpp
+++ Sorting/Main/selection_sort.cpp
@@ -1,12 +1,14 @@
 #include "Header.h"
 
-void selection_sort(int*& a, int n)
+void selection_sort(int*& a, int n, long long& comparisons)
 {
+	comparisons = 0;
 	for (int i = 0; i < n - 1; i++)
 	{
 		int k = i;
 		for (int j = i + 1; j < n; j++)
 		{
+			++comparisons;
 			if (a[k] > a[j])
 				k = j;
 		}
