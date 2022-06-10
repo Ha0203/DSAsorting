75219048f8381efdd06de3d12b7d5380f712b186
ngkhnghyyy <nkhy21@clc.fitus.edu.vn>
2022-06-10 10:20:37 +0700

    Update selection_sort.cpp

--- Sorting/Main/selection_sort.cpp
+++ Sorting/Main/selection_sort.cpp
@@ -3,7 +3,21 @@
 void selection_sort(int*& a, int n,unsigned long long& comparisons)
 {
 	comparisons = 0;
-	for (int i = 0; i < n - 1; i++)
+	int i = 0;
+	while (++comparisons && i < n - 1)
+	{
+		int k = i;
+		int j = i + 1;
+		while (++comparisons && j < n)
+		{
+			if (++comparisons && a[k] > a[j])
+				k = j;
+			j++;
+		}
+		swap(a[k], a[i]);
+		i++;
+	}
+	/*for (int i = 0; i < n - 1; i++)
 	{
 		int k = i;
 		for (int j = i + 1; j < n; j++)
@@ -13,5 +27,5 @@ void selection_sort(int*& a, int n,unsigned long long& comparisons)
 				k = j;
 		}
 		swap(a[k], a[i]);
-	}
+	}*/
 }
