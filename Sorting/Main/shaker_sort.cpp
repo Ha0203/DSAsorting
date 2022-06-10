28feba7069747e21691022e5f675b9a5247155ed
Tu-21127716 <101002686+Tu-21127716@users.noreply.github.com>
2022-06-10 20:15:39 +0700

    Update shaker_sort.cpp

--- Sorting/Main/shaker_sort.cpp
+++ Sorting/Main/shaker_sort.cpp
@@ -5,7 +5,7 @@ void shaker_sort(int a[], int n)
 {
 	int left = 0, right = n - 1;
 	int guard = right;
-	while (left != right)
+	while (left < right)
 	{
 		for (int i = right; i > left; i--)
 			if (a[i] < a[i - 1])
@@ -16,7 +16,7 @@ void shaker_sort(int a[], int n)
 				guard = i;
 			}
 		left = guard;
-		for (int i = 0; i < right; i++)
+		for (int i = left; i < right; i++)
 			if (a[i] > a[i + 1])
 			{
 				int temp = a[i];
