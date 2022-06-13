6b84f043e4e304d51cc5042a0dad5f48910785d1
Ha0203 <mr.stsh020303@gmail.com>
2022-06-13 07:58:55 +0700

    Them bien dem comparisons cho shaker sort

--- Sorting/Main/shaker-sort.cpp
+++ Sorting/Main/shaker-sort.cpp
@@ -1,16 +1,16 @@
 #include"Header.h"
 
-void shaker_sort(int a[], int n) {
+void shaker_sort(int a[], int n, unsigned long long& comparisons) {
 	int left = 0, right = n - 1;
-	while (right - left > 1) {
-		for (int i = left; i < right; i++) {
-			if (a[i] > a[i + 1])
+	while (++comparisons && right - left > 1) {
+		for (int i = left; ++comparisons && i < right; i++) {
+			if (++comparisons && a[i] > a[i + 1])
 				swap(a[i], a[i + 1]);
 		}
 		right--;
-		for (int i = right; i > left; i--)
+		for (int i = right; ++comparisons && i > left; i--)
 		{
-			if (a[i] < a[i - 1])
+			if (++comparisons && a[i] < a[i - 1])
 				swap(a[i], a[i - 1]);
 		}
 		left++;
