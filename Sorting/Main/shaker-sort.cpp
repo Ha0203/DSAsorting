165f4d4b4f0821f309e86527bfe2fa5dd899652c
Ha0203 <mr.stsh020303@gmail.com>
2022-06-13 19:57:25 +0700

    Add DataGenerator and modifi shaker sort

--- Sorting/Main/shaker-sort.cpp
+++ Sorting/Main/shaker-sort.cpp
@@ -1,6 +1,23 @@
 #include"Header.h"
 
-void shaker_sort(int a[], int n, unsigned long long& comparisons) {
+void shaker_sort_ori(int a[], int n) {
+	int left = 0, right = n - 1;
+	while (right - left >= 1) {
+		for (int i = left; i < right; i++) {
+			if (a[i] > a[i + 1])
+				swap(a[i], a[i + 1]);
+		}
+		right--;
+		for (int i = right; i > left; i--)
+		{
+			if (a[i] < a[i - 1])
+				swap(a[i], a[i - 1]);
+		}
+		left++;
+	}
+}
+
+void shaker_sort_count(int a[], int n, unsigned long long& comparisons) {
 	int left = 0, right = n - 1;
 	while (++comparisons && right - left >= 1) {
 		for (int i = left; ++comparisons && i < right; i++) {
