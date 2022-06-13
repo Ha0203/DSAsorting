d6cb3d64f227531fd7232f3367b665189ac060a4
Ha0203 <mr.stsh020303@gmail.com>
2022-06-13 14:18:23 +0700

    fix logic error in shaker-sort

--- Sorting/Main/shaker-sort.cpp
+++ Sorting/Main/shaker-sort.cpp
@@ -2,7 +2,7 @@
 
 void shaker_sort(int a[], int n, unsigned long long& comparisons) {
 	int left = 0, right = n - 1;
-	while (++comparisons && right - left > 1) {
+	while (++comparisons && right - left >= 1) {
 		for (int i = left; ++comparisons && i < right; i++) {
 			if (++comparisons && a[i] > a[i + 1])
 				swap(a[i], a[i + 1]);
